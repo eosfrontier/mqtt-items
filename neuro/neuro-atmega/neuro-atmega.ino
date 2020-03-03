a6273ad19838cf214c7c8d94ffb44f31d067027c
Willem Monsuwe <willem@monsuwe.nl>
2020-03-03 18:08:52 +0100

    Opzetje animatie via hue chroma luma en meer dynamisch

--- neuro/neuro-atmega/neuro-atmega.ino
+++ neuro/neuro-atmega/neuro-atmega.ino
@@ -15,38 +15,123 @@ uint32_t colors[] = {  0x000000,  0x008844,  0x00cccc,  0x0000ff,  0x0000ff,  0x
                        0x000000,  0xff0000,  0x00ff00,  0x0000ff,  0xff0000,  0x00ff00,  0x0000ff,  0x000000 };
 int c_len = 8;
 
-int l_line1[] = { 2,3,4,5,6 };
-int l_line2[] = { 2,3,4,5,12,7,8,9 };
-int l_line3[] = { 2,3,4,13,14,11,10 };
-int l_line4[] = { 2,3,4,13,14,11,15,16 };
-int *l_lines[] = { l_line1, l_line2, l_line3, l_line4 };
-int l_lens[] = { 5, 8, 7, 8 };
+char l_line1[] = { 2,3,4,5,6 };
+char l_line2[] = { 2,3,4,5,12,7,8,9 };
+char l_line3[] = { 2,3,4,13,14,11,10 };
+char l_line4[] = { 2,3,4,13,14,11,15,16 };
+char *l_lines[] = { l_line1, l_line2, l_line3, l_line4 };
+char l_lens[] = { 5, 8, 7, 8 };
 
-int r_line1[] = { 2,3,4,5,6 };
-int r_line2[] = { 2,3,4,12,11,7,8,9 };
-int r_line3[] = { 2,3,4,12,11,10 };
-int r_line4[] = { 2,3,4,12,13,14,15,16 };
-int *r_lines[] = { r_line1, r_line2, r_line3, r_line4 };
-int r_lens[] = { 5, 8, 6, 8 };
+char r_line1[] = { 2,3,4,5,6 };
+char r_line2[] = { 2,3,4,12,11,7,8,9 };
+char r_line3[] = { 2,3,4,12,11,10 };
+char r_line4[] = { 2,3,4,12,13,14,15,16 };
+char *r_lines[] = { r_line1, r_line2, r_line3, r_line4 };
+char r_lens[] = { 5, 8, 6, 8 };
 
-uint32_t interp(uint32_t a, uint32_t b, uint32_t pos) {
-  uint32_t rb = (a) & 0xff;
-  uint32_t gb = (a >> 8) & 0xff;
-  uint32_t bb = (a >> 16) & 0xff;
-  uint32_t ra = (b) & 0xff;
-  uint32_t ga = (b >> 8) & 0xff;
-  uint32_t ba = (b >> 16) & 0xff;
-  uint32_t ri = (ra * pos + rb * (FIXEDP-pos-1))/FIXEDP;
-  uint32_t gi = (ga * pos + gb * (FIXEDP-pos-1))/FIXEDP;
-  uint32_t bi = (ba * pos + bb * (FIXEDP-pos-1))/FIXEDP;
-  return (ri + (gi << 8) + (bi << 16));
+char lines[] = { l_line1, l_line2, l_line3, l_line4, r_line1, r_line2, r_line3, r_line4 };
+char ln_lens[] = { 5,8,7,8,5,8,6,8 };
+
+#define NUM_POINTS 4
+#define NUM_COLS 4
+
+typedef struct {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} rgb_t;
+
+typedef struct {
+    short hue;
+    short chroma;
+    short luma;
+} hcl_t;
+
+struct anim {
+    unsigned long tick;
+    hcl_t hcl[NUM_COLS];
+    short idx[NUM_COLS+1];
+    short spd;
+    unsigned char line; // 1 bit per possible line
+    char ppos1; // Position on line
+    char ppos2;
+    unsigned char mode; //  ??  Bounce oid?
+    char dly;
+} anims[NUM_POINTS];
+
+rgb_t get_hcl_color(hcl_t a, hcl_t b, uint32_t pos, uint32_t tot)
+{
+    long hue = (a.hue * pos + b.hue * (tot-pos-1)) / tot + (36 * FIXEDP); // Is cyclisch, in het positieve trekken (slechte hack maarja)
+    long chroma = (a.chroma * pos + b.chroma * (tot-pos-1)) / tot;
+    long luma = (a.luma * pos + b.luma * (tot-pos-1)) / tot;
+    long x = chroma * (1 - abs(hue % FIXEDP - FIXEDP)) / FIXEDP;
+    long r = 0, g = 0, b = 0;
+    switch ((hue / FIXEDP) % 6) {
+        case 0: r = c; g = x;
+        case 1: r = x; g = c
+        case 2: g = c; b = x;
+        case 3: g = x; b = c;
+        case 4: r = x; b = c;
+        case 5: r = c; b = x;
+    }
+    // Luma 
+    long m = luma - (r * 77 / FIXEDP) - (g * 151 / FIXEDP) - (b * 28 / FIXEDP);
+    r += m;
+    g += m;
+    b += m;
+    if (r > 255) r = 255;
+    if (g > 255) g = 255;
+    if (b > 255) b = 255;
+    if (r < 0) r = 0;
+    if (g < 0) g = 0;
+    if (b < 0) b = 0;
+    rgb_t rgb = { .r = r, .g = g, .b = b };
+    return rgb;
 }
 
-uint32_t get_color(long pos, int coff) {
-  if (pos < 0) return 0x000000;
-  int off = pos/FIXEDP;
-  if (off > (c_len-2)) return 0x000000;
-  return interp(colors[coff+off],colors[coff+off+1], pos % FIXEDP);
+// See if the point is part of a line and if so return the index on that line
+int get_pointindex(int off, int pos, unsigned char *ptline)
+{
+    for (int l = 0; l < 4; l++) {
+        int ln = l+off;
+        if (ptline & (1<<l)) {
+            char *line = lines[ln];
+            for (int i = 0; i < ln_lens[ln]; i++) {
+                if (line[i] == pos) {
+                    return (off ? 16-i : i);
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+// Add the color of all moving points for the given led pixel
+uint32_t get_pix_color(int off, int pos, long tick)
+{
+    short r = 0, g = 0, b = 0;
+    for (int p = 0; p < NUM_POINTS; p++) {
+        struct anim *pt = anims[p];
+        int idx = get_pointindex(off, pos, pt->line) - pt->ppos1;
+        // idx is now integer index of pixel
+        if (idx >= 0 && idx <= (pt->ppos2 - pt->ppos1)) {
+            idx = (idx * FIXEDP) + (tick - pt->tick)*FIXEDP / pt->spd;
+            // idx is now fixedpoint index of pixel relative to point animation
+            if (idx >= 0 && idx < pt->idx[NUM_COLS]) {
+                for (int i = 0; i < NUM_COLS; i++) {
+                    if (pt->idx[i] < idx) { break; }
+                }
+                rgb_t itp = get_hcl_color(pt->hcl[i], pt->hcl[i+1], idx - pt->idx[i], pt->idx[i+1] - pt->idx[i]);
+                r += itp.r;
+                g += itp.g;
+                b += itp.b;
+            }
+        }
+    }
+    if (r > 255) r = 255;
+    if (g > 255) g = 255;
+    if (b > 255) b = 255;
+    return (r + (g << 8) + (b << 16));
 }
 
 void setup() {
@@ -72,29 +157,15 @@ void loop() {
   long cspd = FIXEDP/5;
   // put your main code here, to run repeatedly:
   while (1) {
-    int lline = random(4);
-    int rline = random(4);
-    int lcol = random(3) * c_len;
-    int rcol = random(3) * c_len;
-    int *l_line = l_lines[lline];
-    int *r_line = r_lines[rline];
-    int llen = l_lens[lline];
-    int rlen = r_lens[rline];
-    int len = rlen > llen ? rlen : llen;
-    int cend = -cwid * (c_len-1);
-    int cpos = cwid * len;
-    while (cpos >= cend) {
-      cpos -= cspd;
-      for (int j = 0; j < llen; j++) {
-        l_strip.setPixelColor(l_line[j], get_color(cpos + FIXEDP*j, lcol));
-      }
-      for (int j = 0; j < rlen; j++) {
-        r_strip.setPixelColor(r_line[j], get_color(cpos + FIXEDP*j, rcol));
+      unsigned long now = millis();
+      for (int j = 2; j <= 16; j++) {
+        l_strip.setPixelColor(j, get_pix_color(0, j, now));
+        r_strip.setPixelColor(j, get_pix_color(4, j, now));
       }
       l_strip.show();
       r_strip.show();
       delay(20);
-    }
+      delay(20);
   }
   // Serial.println("Done one round");
 }
