eee201097a60e8fdc0bcfb2c146f3eb8d79112fb
Willem Monsuwe <willem@monsuwe.nl>
2020-03-03 21:22:05 +0100

    Kleine init om te testen

--- neuro/neuro-atmega/neuro-atmega.ino
+++ neuro/neuro-atmega/neuro-atmega.ino
@@ -32,7 +32,7 @@ char r_lens[] = { 5, 8, 6, 8 };
 char lines[] = { l_line1, l_line2, l_line3, l_line4, r_line1, r_line2, r_line3, r_line4 };
 char ln_lens[] = { 5,8,7,8,5,8,6,8 };
 
-#define NUM_POINTS 4
+#define NUM_POINTS 2
 #define NUM_COLS 4
 
 typedef struct {
@@ -47,17 +47,17 @@ typedef struct {
     short luma;
 } hcl_t;
 
-struct anim {
+struct point {
     unsigned long tick;
     hcl_t hcl[NUM_COLS];
-    short idx[NUM_COLS+1];
+    short idx[NUM_COLS];
     short spd;
     unsigned char line; // 1 bit per possible line
     char ppos1; // Position on line
     char ppos2;
     unsigned char mode; //  ??  Bounce oid?
     char dly;
-} anims[NUM_POINTS];
+} points[NUM_POINTS];
 
 rgb_t get_hcl_color(hcl_t a, hcl_t b, uint32_t pos, uint32_t tot)
 {
@@ -74,18 +74,9 @@ rgb_t get_hcl_color(hcl_t a, hcl_t b, uint32_t pos, uint32_t tot)
         case 4: r = x; b = c;
         case 5: r = c; b = x;
     }
-    // Luma 
+    // Luma : 0.3*R + 0.59*G + 0.11*B
     long m = luma - (r * 77 / FIXEDP) - (g * 151 / FIXEDP) - (b * 28 / FIXEDP);
-    r += m;
-    g += m;
-    b += m;
-    if (r > 255) r = 255;
-    if (g > 255) g = 255;
-    if (b > 255) b = 255;
-    if (r < 0) r = 0;
-    if (g < 0) g = 0;
-    if (b < 0) b = 0;
-    rgb_t rgb = { .r = r, .g = g, .b = b };
+    rgb_t rgb = { .r = constrain(r+m, 0, 255), .g = constrain(g+m, 0, 255), .b = constrain(b+m, 0, 255) };
     return rgb;
 }
 
@@ -111,30 +102,70 @@ uint32_t get_pix_color(int off, int pos, long tick)
 {
     short r = 0, g = 0, b = 0;
     for (int p = 0; p < NUM_POINTS; p++) {
-        struct anim *pt = anims[p];
+        struct point *pt = points[p];
         int idx = get_pointindex(off, pos, pt->line) - pt->ppos1;
         // idx is now integer index of pixel
         if (idx >= 0 && idx <= (pt->ppos2 - pt->ppos1)) {
-            idx = (idx * FIXEDP) + (tick - pt->tick)*FIXEDP / pt->spd;
+            idx = (idx * FIXEDP) - ((tick - pt->tick - (pt->time/2))*pt->spd / FIXEDP);
             // idx is now fixedpoint index of pixel relative to point animation
-            if (idx >= 0 && idx < pt->idx[NUM_COLS]) {
-                for (int i = 0; i < NUM_COLS; i++) {
-                    if (pt->idx[i] < idx) { break; }
+            if (idx >= pt->idx[0] && idx < pt->idx[NUM_COLS-1]) {
+                int i;
+                for (i = 1; i < NUM_COLS-1; i++) {
+                    if (idx < pt->idx[i]) { break; }
                 }
-                rgb_t itp = get_hcl_color(pt->hcl[i], pt->hcl[i+1], idx - pt->idx[i], pt->idx[i+1] - pt->idx[i]);
+                rgb_t itp = get_hcl_color(pt->hcl[i-1], pt->hcl[i], idx - pt->idx[i-1], pt->idx[i] - pt->idx[i-1]);
                 r += itp.r;
                 g += itp.g;
                 b += itp.b;
             }
         }
     }
-    if (r > 255) r = 255;
-    if (g > 255) g = 255;
-    if (b > 255) b = 255;
-    return (r + (g << 8) + (b << 16));
+    return (min(r, 255) + (min(g, 255) << 8) + (min(b, 255) << 16));
 }
 
-void setup() {
+void set_point(int p, unsigned long now, short hue, unsigned char line)
+{
+    struct point *pt = points[p];
+    pt->tick = now;
+
+    pt->idx[0] = -500;
+    pt->hcl[0].hue = hue;
+    pt->hcl[0].chroma = 0xff;
+    pt->hcl[0].luma = 0;
+    pt->idx[1] = -300;
+    pt->hcl[1].hue = hue+0x100;
+    pt->hcl[1].chroma = 0xff;
+    pt->hcl[1].luma = 0xff;
+    pt->idx[2] = 300;
+    pt->hcl[2].hue = hue+0x200;
+    pt->hcl[2].chroma = 0xff;
+    pt->hcl[2].luma = 0xff;
+    pt->idx[3] = 500;
+    pt->hcl[3].hue = hue+0x300;
+    pt->hcl[3].chroma = 0xff;
+    pt->hcl[3].luma = 0;
+
+    pt->spd = 1000;
+    pt->time = 16000;
+    pt->line = line;
+    pt->ppos1 = 0;
+    pt->ppos2 = 15;
+    pt->mode = 0;
+    pt->dly = 0;
+}
+
+void update_points(unsigned long now)
+{
+    for (int p = 0; p < NUM_POINTS; p++) {
+        if (points[p].tick + points[p].time <= now) {
+            points[p].now += points[p].time;
+            points[p].spd = -points[p].spd;
+        }
+    }
+}
+
+void setup()
+{
   // put your setup code here, to run once:
   l_strip.begin();
   l_strip.setPixelColor(0, 0xff0000);
@@ -150,23 +181,23 @@ void setup() {
   //delay(4000);
   //Serial.println("DBG");
   //Serial.println(sizeof(long), DEC);
+  unsigned long now = millis();
+  set_point(0, now, 0x200, 0b10001000);
+  set_point(1, now, 0x500, 0b00010001);
 }
 
-void loop() {
-  long cwid = FIXEDP*3/2;
-  long cspd = FIXEDP/5;
+void loop()
+{
   // put your main code here, to run repeatedly:
-  while (1) {
-      unsigned long now = millis();
-      for (int j = 2; j <= 16; j++) {
-        l_strip.setPixelColor(j, get_pix_color(0, j, now));
-        r_strip.setPixelColor(j, get_pix_color(4, j, now));
-      }
-      l_strip.show();
-      r_strip.show();
-      delay(20);
-      delay(20);
+  unsigned long now = millis();
+  update_points(now);
+  for (int j = 2; j <= 16; j++) {
+    l_strip.setPixelColor(j, get_pix_color(0, j, now));
+    r_strip.setPixelColor(j, get_pix_color(4, j, now));
   }
+  l_strip.show();
+  r_strip.show();
+  delay(20);
   // Serial.println("Done one round");
 }
 
