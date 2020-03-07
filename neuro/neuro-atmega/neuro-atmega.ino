c64afdaf4576f98de06f0373834d9dda8a0a4925
Willem Monsuwe <willem@monsuwe.nl>
2020-03-07 16:50:07 +0100

    Kleuren en beweging en snelheid nu beter werkend (met knoppen draaien)

--- neuro/neuro-atmega/neuro-atmega.ino
+++ neuro/neuro-atmega/neuro-atmega.ino
@@ -5,34 +5,32 @@
 
 #define LED_COUNT 17
 
-#define FIXEDP 256
+#define FIXEDP ((long)256)
 
 Adafruit_NeoPixel l_strip(LED_COUNT, L_LED_PIN, NEO_RGB + NEO_KHZ800);
 Adafruit_NeoPixel r_strip(LED_COUNT, R_LED_PIN, NEO_RGB + NEO_KHZ800);
 
-int c_len = 8;
-
-char l_line1[] = { 2,3,4,5,6 };
-char l_line2[] = { 2,3,4,5,12,7,8,9 };
-char l_line3[] = { 2,3,4,13,14,11,10 };
-char l_line4[] = { 2,3,4,13,14,11,15,16 };
+const char l_line1[] = { 2,3,4,5,6 };
+const char l_line2[] = { 2,3,4,5,12,7,8,9 };
+const char l_line3[] = { 2,3,4,13,14,11,10 };
+const char l_line4[] = { 2,3,4,13,14,11,15,16 };
 // char *l_lines[] = { l_line1, l_line2, l_line3, l_line4 };
 // char l_lens[] = { 5, 8, 7, 8 };
 
-char r_line1[] = { 2,3,4,5,6 };
-char r_line2[] = { 2,3,4,12,11,7,8,9 };
-char r_line3[] = { 2,3,4,12,11,10 };
-char r_line4[] = { 2,3,4,12,13,14,15,16 };
+const char r_line1[] = { 2,3,4,5,6 };
+const char r_line2[] = { 2,3,4,12,11,7,8,9 };
+const char r_line3[] = { 2,3,4,12,11,10 };
+const char r_line4[] = { 2,3,4,12,13,14,15,16 };
 // char *r_lines[] = { r_line1, r_line2, r_line3, r_line4 };
 // char r_lens[] = { 5, 8, 6, 8 };
 
-char *lines[] = { l_line1, l_line2, l_line3, l_line4, r_line1, r_line2, r_line3, r_line4 };
-char ln_lens[] = { 5,8,7,8,5,8,6,8 };
+const char *lines[] = { l_line1, l_line2, l_line3, l_line4, r_line1, r_line2, r_line3, r_line4 };
+const char ln_lens[] = { 5,8,7,8,5,8,6,8 };
 
-char inpins[] = { 3,4,5,6,8,9,14,15,16,18,19,20 };
+const char inpins[] = { 3,4,5,6,8,9,14,15,16,18,19,20 };
 
 #define NUM_POINTS 2
-#define NUM_COLS 4
+#define NUM_COLS 5
 
 typedef struct {
     uint32_t r;
@@ -48,7 +46,7 @@ typedef struct {
 
 struct point {
     unsigned long tick;
-    long time;
+    long size;
     hcl_t hcl[NUM_COLS];
     short idx[NUM_COLS];
     short spd;
@@ -62,6 +60,14 @@ struct point {
 
 rgb_t get_hcl_color(hcl_t ca, hcl_t cb, uint32_t pos, uint32_t tot)
 {
+    debug_p("hue1", ca.hue);
+    debug_p("chroma1", ca.chroma);
+    debug_p("luma1", ca.luma);
+    debug_p("hue2", cb.hue);
+    debug_p("chroma2", cb.chroma);
+    debug_p("luma2", cb.luma);
+    debug_p("pos", pos);
+    debug_p("tot", tot);
     long hue =    (cb.hue    * pos + ca.hue    * (tot-pos-1)) / tot + (36 * FIXEDP); // Is cyclisch, in het positieve trekken (slechte hack maarja)
     long chroma = (cb.chroma * pos + ca.chroma * (tot-pos-1)) / tot;
     long luma =   (cb.luma   * pos + ca.luma   * (tot-pos-1)) / tot;
@@ -75,11 +81,40 @@ rgb_t get_hcl_color(hcl_t ca, hcl_t cb, uint32_t pos, uint32_t tot)
         case 4: r = x; b = chroma; break;
         case 5: r = chroma; b = x; break;
     }
+    debug_p("hue", hue);
+    debug_p("chroma", chroma);
+    debug_p("luma", luma);
     // Luma : 0.3*R + 0.59*G + 0.11*B
     long m = luma - (r * 77 / FIXEDP) - (g * 151 / FIXEDP) - (b * 28 / FIXEDP);
-    rgb_t rgb = { .r = constrain(r+m, 0, 255), .g = constrain(g+m, 0, 255), .b = constrain(b+m, 0, 255) };
+    r += m;
+    g += m;
+    b += m;
+    debug_p("r", r);
+    debug_p("g", g);
+    debug_p("b", b);
+    if (r < 0) r = 0;
+    if (r > 255) r = 255;
+    if (g < 0) g = 0;
+    if (g > 255) g = 255;
+    if (b < 0) b = 0;
+    if (b > 255) b = 255;
+    rgb_t rgb = { .r = r, .g = g, .b = b };
+    //rgb_t rgb = { .r = constrain(r+m, 0, 255), .g = constrain(g+m, 0, 255), .b = constrain(b+m, 0, 255) };
     return rgb;
 }
+
+bool debugging = false;
+
+void debug_p(char *msg, long val)
+{
+    if (debugging) {
+        Serial.print(msg);
+        Serial.print(" ");
+        Serial.println(val);
+        delay(1);
+    }
+}
+
 
 // See if the point is part of a line and if so return the index on that line
 int get_pointindex(int off, int pos, unsigned char ptline)
@@ -104,16 +139,34 @@ uint32_t get_pix_color(int off, int pos, long tick)
     uint32_t r = 0, g = 0, b = 0;
     for (int p = 0; p < NUM_POINTS; p++) {
         struct point *pt = &points[p];
-        int idx = get_pointindex(off, pos, pt->line) - pt->ppos1;
+        long idx = get_pointindex(off, pos, pt->line) - pt->ppos1;
         // idx is now integer index of pixel
+        debug_p("pixel", pos);
+        debug_p("idx", idx);
         if (idx >= 0 && idx <= (pt->ppos2 - pt->ppos1)) {
-            idx = (idx * FIXEDP) - (pt->ppos2-pt->ppos1)*(FIXEDP/2) - ((tick - pt->tick - (pt->time/2))*(pt->spd*pt->dir) / FIXEDP);
+            debug_p("idx (1.5)", (idx * FIXEDP) - (pt->ppos2-pt->ppos1)*(FIXEDP/2));
+            debug_p("pt->ppos1", pt->ppos1);
+            debug_p("pt->ppos2", pt->ppos2);
+            debug_p("idx * FIXEDP", idx * FIXEDP);
+            debug_p("(pt->ppos2-pt->ppos1)*(FIXEDP/2)", (pt->ppos2-pt->ppos1)*(FIXEDP/2));
+            debug_p("(idx * FIXEDP) - (pt->ppos2-pt->ppos1)*(FIXEDP/2)", (idx * FIXEDP) - (pt->ppos2-pt->ppos1)*(FIXEDP/2));
+            debug_p("(((tick - pt->tick) * pt->spd) / 1024) * pt->dir", (((tick - pt->tick) * pt->spd) / 1024) * pt->dir);
+            debug_p("(pt->size/2)", (pt->size/2));
+            idx = (idx * FIXEDP) - (pt->ppos2-pt->ppos1)*(FIXEDP/2) - (((tick - pt->tick) * pt->spd) / 1024 - (pt->size/2)) * pt->dir;
+            debug_p("idx (2)", idx);
+            debug_p("tick - pt->tick", tick - pt->tick);
+            debug_p("pt->spd * pt->dir", pt->spd * pt->dir);
+            debug_p("(((tick - pt->tick) * pt->spd * pt->dir))",        (((tick - pt->tick) * pt->spd * pt->dir)));
+            debug_p("(((tick - pt->tick) * pt->spd * pt->dir) / 1024)", (((tick - pt->tick) * pt->spd) / 1024) * pt->dir);
+            // Serial.print("Time "); Serial.print(tick - pt->tick); Serial.print(" offset index="); Serial.println(idx);
             // idx is now fixedpoint index of pixel relative to point animation
             if (idx >= pt->idx[0] && idx < pt->idx[NUM_COLS-1]) {
                 int i;
                 for (i = 1; i < NUM_COLS-1; i++) {
                     if (idx < pt->idx[i]) { break; }
                 }
+                debug_p("color idx", i);
+                // Serial.print("Color number "); Serial.print(i); Serial.print(" ");
                 rgb_t itp = get_hcl_color(pt->hcl[i-1], pt->hcl[i], idx - pt->idx[i-1], pt->idx[i] - pt->idx[i-1]);
                 r += itp.r;
                 g += itp.g;
@@ -140,18 +193,22 @@ void set_point(int p, unsigned long now, short hue, unsigned char line, int dir)
     pt->hcl[1].hue = hue+0x580;
     pt->hcl[1].chroma = 0xff;
     pt->hcl[1].luma = 0;
-    pt->idx[2] = 600;
-    pt->hcl[2].hue = hue+0x680;
+    pt->idx[2] = 0;
+    pt->hcl[2].hue = hue+0x600;
     pt->hcl[2].chroma = 0xff;
-    pt->hcl[2].luma = 0;
-    pt->idx[3] = 1500;
-    pt->hcl[3].hue = hue+0x700;
-    pt->hcl[3].chroma = 0;
+    pt->hcl[2].luma = 0xff;
+    pt->idx[3] = 600;
+    pt->hcl[3].hue = hue+0x680;
+    pt->hcl[3].chroma = 0xff;
     pt->hcl[3].luma = 0;
+    pt->idx[4] = 1500;
+    pt->hcl[4].hue = hue+0x700;
+    pt->hcl[4].chroma = 0;
+    pt->hcl[4].luma = 0;
 
-    pt->spd = 800;
+    pt->spd = 4 * FIXEDP;
     pt->dir = dir;
-    pt->time = 4*FIXEDP;
+    pt->size = 16 * FIXEDP + 3000;
     pt->line = line;
     pt->ppos1 = 0;
     pt->ppos2 = 16;
@@ -161,32 +218,44 @@ void set_point(int p, unsigned long now, short hue, unsigned char line, int dir)
 
 void update_points(unsigned long now)
 {
-    for (int p = 0; p < NUM_POINTS; p++) {
-        uint32_t aspd = (points[p].spd * points[p].time) / FIXEDP;
-        if (points[p].tick + aspd <= now) {
-            points[p].tick += aspd;
-            points[p].dir = -points[p].dir;
-        }
+  for (int p = 0; p < NUM_POINTS; p++) {
+    if (points[p].spd > 0) {
+      debug_p("size",points[p].size);
+      debug_p("spd", points[p].spd);
+      uint32_t aspd = (points[p].size * 1024 / (points[p].spd));
+      debug_p("aspd", aspd);
+      if (points[p].tick + aspd <= now) {
+          // Serial.print("Updating "); Serial.print(p); Serial.print(" : tick = "); Serial.print(points[p].tick);
+          // Serial.print(" now = "); Serial.print(now); Serial.print(" dir = "); Serial.println((int)points[p].dir);
+          points[p].tick += aspd;
+          points[p].dir = -points[p].dir;
+      }
     }
+  }
 }
 
 #define DEBOUNCE 100
 
+void rotate_hue(unsigned char dir)
+{
+  
+}
+
 void rotate_knob(int pos, unsigned char dir)
 {
-  Serial.print("Rotate knob ");
-  Serial.print(pos);
-  Serial.println(dir ? " Right" : " Left");
+  // Serial.print("Rotate knob ");
+  // Serial.print(pos);
+  // Serial.println(dir ? " Right" : " Left");
   switch(pos) {
     case 0:
       for (int i = 0; i < NUM_POINTS; i++) {
         if (dir) {
-          if (points[i].spd < 2000) {
-            points[i].spd += 10;
+          if (points[i].spd < (10*FIXEDP)) {
+            points[i].spd += 16;
           }
         } else {
-          if (points[i].spd > 100) {
-            points[i].spd -= 10;
+          if (points[i].spd > (FIXEDP/4)) {
+            points[i].spd -= 16;
           }
         }
       }
@@ -219,8 +288,8 @@ void rotate_knob(int pos, unsigned char dir)
 
 void press_knob(int pos)
 {
-  Serial.print("Press knob ");
-  Serial.println(pos);
+  // Serial.print("Press knob ");
+  // Serial.println(pos);
 }
 
 void process_knob(unsigned int pos, unsigned char rot1, unsigned char rot2, unsigned char btn)
@@ -251,7 +320,6 @@ void process_knob(unsigned int pos, unsigned char rot1, unsigned char rot2, unsi
     //   0100 = 0   1
     if (rot != r2) {
       // Niet op en neer
-      // Serial.print("DBG "); Serial.print(rot); Serial.print(" "); Serial.print(r1); Serial.print(" "); Serial.print(r2); Serial.print(" = "); Serial.println(rot2 ^ (r1&1));
       rotate_knob(pos, rot2 ^ (r1 & 1));
     }
     knob_r2 = (knob_r2 & ~(3 << (2*pos))) | r1 << (2*pos);
@@ -288,11 +356,12 @@ void loop()
     }
     l_strip.show();
     r_strip.show();
+    debugging = false;
     usecount++;
     unsigned long elaps = millis() - now;
     usesum += elaps;
-    if (elaps < 20) {
-      now += 20;
+    if (elaps < 500) {
+      now += 500;
     } else {
       now += elaps;
     }
@@ -326,10 +395,22 @@ void setup()
     pinMode(inpins[i], INPUT_PULLUP);
   }
   
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println("DBG");
+  if (debugging) {
+      Serial.begin(9600);
+      delay(2000);
+      Serial.println("Starting");
+  }
+  /*
+  for (int i = 0; i < 100; i++) {
+  delay(100);
+  Serial.print(millis());
+  Serial.print(" ");
+  Serial.println(i);
+  }
+  */
   unsigned long now = millis();
-  set_point(0, now, 0x000, 0b11000110,  1);
-  set_point(1, now, 0x200, 0b01101100, -1);
+  //set_point(0, now, 0x000, 0b11000110,  1);
+  //set_point(1, now, 0x200, 0b01101100, -1);
+  set_point(0, now-3000, 0x000, 0b10000010,  1);
+  set_point(1, now-3000, 0x200, 0b00101000, -1);
 }
